4115d9ae8aae5e34fbe40f65aec0f3ceab9a09e4
Oleksandra Zarubina <aleksandra.zarubina20@gmail.com>
2026-03-14 23:10:35 +0200

    Fix more comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const form = document.getElementById('contacts-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (form.checkValidity()) {
+    form.reset();
+  } else {
+    form.reportValidity();
+  }
+});
